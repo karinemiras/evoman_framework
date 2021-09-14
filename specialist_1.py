3f075b721083870c15ab8607cffcb5cfa083d1b2
Dymitr <dymitrlubczyk@gmail.com>
2021-09-14 12:31:57 +0200

    Add demo file for EvolutionaryAlgorithm class

--- specialist_1.py
+++ specialist_1.py
@@ -1,28 +1,50 @@
-import sys
-sys.path.insert(0, 'evoman')
-
-from environment import Environment
-from demo_controller import player_controller
+from typing import NewType
 import numpy as np
-import os
+from base_evolutionary_algorithm import EvolutionaryAlgorithm
 
 
-experiment_name = 'individual_1'
-if not os.path.exists(experiment_name):
-    os.makedirs(experiment_name)
+def selection(fitness, population):
+    i = np.flip(np.argsort(fitness), axis=None)
+
+    return population[i[:(i.size // 5)], :]
 
 
-# initializes simulation in individual evolution mode, for single static enemy.
-env = Environment(experiment_name=experiment_name,
-                  enemies=[1],
-                  playermode="ai",
-                  player_controller=player_controller(0),  # no hidden layer
-                  enemymode="static",
-                  level=2,
-                  speed="normal")
+def crossover(selected_individuals):
+    selected_count = selected_individuals.shape[0]
+    offsprings = np.array([])
+    offspring_count = selected_count // 2
+    genome_length = selected_individuals.shape[1]
 
-# 5 is a number of possible outputs
-# env.get_num_sensors() is input size, +1 for bias
-genome_length = 5 * (env.get_num_sensors() + 1)
-genome = np.random.uniform(-1, 1, size=(genome_length,))
-env.play(pcont=genome)
+    for i in range(offspring_count):
+        partner = np.random.randint(selected_count)
+        crossing_point = np.random.randint(genome_length)
+
+        father_genes = selected_individuals[i, : crossing_point]
+        mother_genes = selected_individuals[partner, crossing_point:]
+
+        offspring = np.concatenate((father_genes, mother_genes), axis=None)
+        offsprings = np.concatenate((offsprings, offspring), axis=None)
+
+    mutants_count = selected_count // 10
+    mutants = np.random.uniform(-1, 1, mutants_count * genome_length)
+
+    newcomers_count = mutants_count + offspring_count
+    return np.concatenate((offsprings, mutants), axis=None).reshape(newcomers_count, genome_length)
+
+
+def insertion(fitness, population, newcomers):
+    i = np.argsort(fitness, axis=None)
+    population = np.delete(population, i[: newcomers.shape[0]], 0)
+    return np.concatenate((population, newcomers))
+
+
+evolutionaryAlgorithm = EvolutionaryAlgorithm(_experiment_name='solution1',
+                                              _population_size=100,
+                                              _generations_number=50,
+                                              _selection=selection,
+                                              _crossover=crossover,
+                                              _mutation=1,
+                                              _insertion=insertion)
+
+
+controller = evolutionaryAlgorithm.findSolution()
