cd63a4dc8945ade178f799d789b9abb01dd3e040
Nilss98 <breeman.nb@gmail.com>
2021-09-15 17:20:27 +0200

    add tasks

--- evolution.py
+++ evolution.py
@@ -14,40 +14,49 @@ def initialize_generation(population_size, num_genes):
 	return np.random.uniform(-1, 1, (population_size, num_genes))
 
 
-def generate_next_generation(population_fitness):
+def generate_next_generation(population_array, fitness_array):
 	"""
+	Nils
 	Generates next generation from current population.
 	:param population_fitness: array containing each individual in population and their fitness score
 	"""
 	# should normalize each new vector between [-1, 1]
 	return
 
-def parent_selection(population_fitness):
+def parent_selection(population_array, fitness_array):
 	"""
+	Charles
 	Returns a list of pairs with size |population| containing the selected parents that will reproduce.
 	:param populaton_fitness: array containing each individual in population and their fitness score
 	"""
 	return
 
-def recombine(x, y):
+def recombine(parent_1, parent_2):
 	"""
+	Nils
 	Generate one offspring from individuals x and y using crossover.
 	:param x: first individual ; numpy vector with 265 weights
 	:param y: second individual ; numpy vector with 265 weights
 	"""
 	return
 
-def mutate(x):
+def mutate(individual):
 	"""
+	Johanna
 	Applies random mutation to individual.
 	:param x: numpy vector with 265 weights
 	"""
 	return
 
-def compute_fitness(env, x):
+def compute_fitness(environment, individual):
 	"""
 	Evaluate the fitness of individual x.
 	:param x: individual x
 	"""
-	fitness, player_life, enemy_life, time = env.play(pcont=x)
+	fitness, player_life, enemy_life, time = environment.play(pcont=individual)
 	return fitness
+
+def surivival_selection():
+	"""
+	Otto
+	"""
