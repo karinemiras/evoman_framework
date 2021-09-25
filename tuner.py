8155310c4a39df792ee029f3e39a1e770fdd391d
Dymitr <dymitrlubczyk@gmail.com>
2021-09-25 11:54:27 +0200

    Fix alpha values

--- tuner.py
+++ tuner.py
@@ -8,31 +8,31 @@ from mutation_selection import MutationSelection
 
 
 class Tuner:
-    steps_count = 4
+    steps_count = 3
     evolutionary_algorithm_runs = 3
 
     def __init__(self, _evolutionary_algorithm):
         self.evolutionary_algorithm = _evolutionary_algorithm
 
     def run(self):
-        for alpha in [40, 20, 10, 5]:
-            print(f'Range: +/-{alpha}%')
+        for alpha in [0.4, 0.2, 0.1, 0.05]:
+            print(f'Range: +/-{100*alpha}%')
             self.tune_parameter(Mutation.mutation_ratio, alpha, self.set_mutation_ratio)
             print('Mutation ratio - ', Mutation.mutation_ratio)
             self.tune_parameter(Selection.selection_ratio, alpha, self.set_selection_ratio)
             print('Selection ratio -', Selection.selection_ratio)
             self.tune_parameter(Crossover.offspring_ratio, alpha, self.set_offspring_ratio)
             print('Offspring ratio - ', Crossover.offspring_ratio)
-            self.tune_parameter(MutationSelection.mutation_selection_ratio,
+            self.tune_parameter(MutationSelection.selection_ratio,
                                 alpha, self.set_mutation_selection_ratio)
-            print('Selection mutation ratio - ', MutationSelection.mutation_selection_ratio)
+            print('Selection mutation ratio - ', MutationSelection.selection_ratio)
 
             print('\n\nCurrent results:')
 
             print('Mutation ratio - ', Mutation.mutation_ratio)
             print('Selection ratio -', Selection.selection_ratio)
             print('Offspring ratio - ', Crossover.offspring_ratio)
-            print('Selection mutation ratio - ', MutationSelection.mutation_selection_ratio)
+            print('Selection mutation ratio - ', MutationSelection.selection_ratio)
             print('\n\n\n')
 
     def tune_parameter(self, current_value, alpha, setter):
@@ -41,10 +41,9 @@ class Tuner:
         best_value = 0
         best_score = 0
 
-        values = np.linspace(bottom, top, Tuner.steps_count)
-        values = np.append(values, current_value)
+        print(np.linspace(bottom, top, Tuner.steps_count))
 
-        for value in values:
+        for value in np.linspace(bottom, top, Tuner.steps_count):
             setter(value)
             score = self.test_algorithm()
             if(score > best_score):
@@ -68,7 +67,7 @@ class Tuner:
         Selection.selection_ratio = value
 
     def set_mutation_selection_ratio(self, value):
-        MutationSelection.mutation_selection_ratio = value
+        MutationSelection.selection_ratio = value
 
     def set_offspring_ratio(self, value):
         Crossover.offspring_ratio = value
