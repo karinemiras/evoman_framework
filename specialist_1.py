5e2a0a7ba7ce8fda3ddd31be0c89444484b4e34b
dymitrlubczyk <56291334+dymitrlubczyk@users.noreply.github.com>
2021-09-25 12:05:08 +0200

    🐉 Tuning improvements (#13)
    * Same initial population
    
    * Minor improvements
    
    * Fix alpha values
    
    * Fix

--- specialist_1.py
+++ specialist_1.py
@@ -18,8 +18,8 @@ from mutation_selection import MutationSelection
 
 
 # HYPERPARAMS
-population_size = 50
-generations_number = 7
+population_size = 100
+generations_number = 20
 
 evolutionary_algorithm = EvolutionaryAlgorithm(_experiment_name='solution_1',
                                                _population_size=population_size,
@@ -32,10 +32,11 @@ evolutionary_algorithm = EvolutionaryAlgorithm(_experiment_name='solution_1',
                                                _mutation_selection=MutationSelection.only_parents,
                                                _insertion=Insertion.basic)
 
-Mutation.mutation_ratio = 0.05
-Crossover.offspring_ratio = 1
-Selection.selection_ratio = 0.3
-MutationSelection.selection_ratio = 0.5
+Mutation.mutation_ratio = 0.0924
+Crossover.offspring_ratio = 0.792
+Selection.selection_ratio = 0.3969
+MutationSelection.selection_ratio = 0.3078
 
-tuner = Tuner(evolutionary_algorithm)
-tuner.run()
+evolutionary_algorithm.run()
+# tuner = Tuner(evolutionary_algorithm)
+# tuner.run()
