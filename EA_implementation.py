f58d2b091b36ece75bd581bd1c1ef6cdcb1bb213
shin <xchen_2020@hotmail.com>
2021-09-26 11:58:10 +0200

    rewrite crossover function

--- EA_implementation.py
+++ EA_implementation.py
@@ -77,8 +77,8 @@ def crossover(parent_1, parent_2, crossover_rate):
     # randomly select whether to perform crossover
     if np.random.rand() < crossover_rate:
         crossover_point = np.random.randint(1, len(parent_1))
-        off_spring_1= np.append(parent_1[:crossover_point], parent_2[crossover_point:])
-        off_spring_2 = np.append(parent_2[:crossover_point], parent_1[crossover_point:])
+        off_spring_1= np.append(parent_1[crossover_point:], parent_2[:crossover_point])
+        off_spring_2 = np.append(parent_2[crossover_point:], parent_1[:crossover_point])   
     else:
         off_spring_1, off_spring_2 = parent_1.copy(), parent_2.copy()
     
