37b5ffdf2f4174cbbb4e87ec70873afa758b67e7
SlayerOfTheBad <jacob.schoemaker2000@gmail.com>
2021-10-27 23:08:53 +0200

    Multithreading?

--- gym_env_test.py
+++ gym_env_test.py
@@ -26,22 +26,22 @@ for env in environments:
     i += 1
 
     model.set_env(env)
-    model.learn(total_timesteps=(2 ** 16))
+    model.learn(total_timesteps=(2 ** 13))
 
     print(f'\n\n\nFinished learning env{i}!\n\n\n')
 
-    env.env.keep_frames = True
-    obs = env.reset()
-
-    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    fps = 30
-    video_filename = f'env{i}_({fsn}, 10,2).avi'
-    out = cv2.VideoWriter(video_filename, fourcc, fps, (env.WIDTH, env.HEIGHT))
-    for _ in range(2500):
-        action, _state = model.predict(obs, deterministic=False)
-        obs, reward, done, info = env.step(action)
-        if done:
-            for frame in env.render('video'):
-                out.write(frame)
-            break
-    out.release()
+    # env.env.keep_frames = True
+    # obs = env.reset()
+    #
+    # fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    # fps = 30
+    # video_filename = f'env{i}_({fsn}, 10,2).avi'
+    # out = cv2.VideoWriter(video_filename, fourcc, fps, (env.WIDTH, env.HEIGHT))
+    # for _ in range(2500):
+    #     action, _state = model.predict(obs, deterministic=False)
+    #     obs, reward, done, info = env.step(action)
+    #     if done:
+    #         for frame in env.render('video'):
+    #             out.write(frame)
+    #         break
+    # out.release()
