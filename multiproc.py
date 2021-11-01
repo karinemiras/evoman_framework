8d1de6457cd43ed2c2f063e62d57769510ece63d
SlayerOfTheBad <jacob.schoemaker2000@gmail.com>
2021-11-01 14:08:12 +0100

    Multithreading?

--- multiproc.py
+++ multiproc.py
@@ -1,7 +1,7 @@
 from subprocess import Popen
 
 processes = []
-for i in range(1, 4):
+for i in range(1, 5):
     processes.append(Popen(["python3", "./gym_env_test.py", str(i)]))
 
 for process in processes:
