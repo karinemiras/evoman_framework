69516ef1c4cb6d2a2fd381bc3f46f14be678bfd2
karinemiras <karine.smiras@gmail.com>
2022-03-21 21:46:22 +0100

    Delete evoman directory

--- evoman/roles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="plataform2" tilewidth="32" tileheight="32">
- <image source="evoman/images/plataform2.jpg" width="150" height="93"/>
-</tileset>
