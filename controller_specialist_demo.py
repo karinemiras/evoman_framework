1a5c7a7edcc0bd75ded353a6cdb7d08bf9275197
julian <julpo99@icloud.com>
2023-09-07 19:16:50 +0200

    Bug Fix: One line added to controller_generalist_demo.py Now the demo should work properly

--- controller_specialist_demo.py
+++ controller_specialist_demo.py
@@ -24,21 +24,19 @@ n_hidden_neurons = 0
 
 # initializes environment for single objective mode (specialist)  with static enemy and ai player
 env = Environment(experiment_name=experiment_name,
-				  playermode="ai",
-				  player_controller=player_controller(n_hidden_neurons),
-			  	  speed="normal",
-				  enemymode="static",
-				  level=2,
-				  visuals=True)
-
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  speed="normal",
+                  enemymode="static",
+                  level=2,
+                  visuals=True)
 
 # tests saved demo solutions for each enemy
 for en in range(1, 9):
+    # Update the enemy
+    env.update_parameter('enemies', [en])
 
-	#Update the enemy
-	env.update_parameter('enemies',[en])
-
-	# Load specialist controller
-	sol = np.loadtxt('solutions_demo/demo_'+str(en)+'.txt')
-	print('\n LOADING SAVED SPECIALIST SOLUTION FOR ENEMY '+str(en)+' \n')
-	env.play(sol)
+    # Load specialist controller
+    sol = np.loadtxt('solutions_demo/demo_' + str(en) + '.txt')
+    print('\n LOADING SAVED SPECIALIST SOLUTION FOR ENEMY ' + str(en) + ' \n')
+    env.play(sol)
