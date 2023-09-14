635254d0c655a5995ebb5bd34e526adc776b6bc4
Julian <julpo99@icloud.com>
2023-09-14 18:57:11 +0200

    Bug Fix Evoman Folder. .gitignore update

--- dummy_demo.py
+++ dummy_demo.py
@@ -14,6 +14,7 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 # initializes environment with ai player using random controller, playing against static enemy
-env = Environment(experiment_name=experiment_name)
+env = Environment(experiment_name=experiment_name, speed="normal", visuals=True)
+#env.update_parameter('enemies', [1])
 env.play()
 
