7b562e95b0d67aec4538a31d0ffd8c556f7d6000
Julian <julpo99@icloud.com>
2023-10-17 12:26:14 +0200

    one change reverted

--- optimization_generalist_pso.py
+++ optimization_generalist_pso.py
@@ -160,6 +160,4 @@ def updateParticle(part, best, w, phi1, phi2):
 
 
 if __name__ == "__main__":
-    #main()
-    logger = DataVisualizer(EXPERIMENT_NAME)
-    logger.draw_both_box_plots(enemies)
+    main()
